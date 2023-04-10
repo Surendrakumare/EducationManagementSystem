d7efd7d2721b6b4f861a5e1f10b713e6a0a1369c
Surendrakumar E <surendrakumar.e@sysforetech.local>
2023-04-10 16:21:30 +0530

    adding tables

---
+++ src/main/java/com/sysfore/demo/educationSitedto/FeedBack.java
@@ -0,0 +1,11 @@
+package com.sysfore.demo.educationSitedto;
+
+import lombok.Data;
+
+@Data
+public class FeedBack {
+	private int feedbackId;
+	private String studentId;
+	private String feedback;
+
+}
